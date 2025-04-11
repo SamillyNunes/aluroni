833c7eb7ceffe5e065b3f4898679165fb001ce42
Samilly <samillynunes19@gmail.com>
2025-04-11 10:52:04 -0300

    style: header added to all pages

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,23 @@ import NavMenu from 'components/NavMenu';
 import Cardapio from 'pages/Cardapio';
 import Home from 'pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styles from './Routes.module.scss';
 
-export default function AppRouter(){
+export default function AppRouter() {
     return (
-        <BrowserRouter>
-            <NavMenu/>
-            <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/menu' element={<Cardapio/>} />
-            </Routes>
-        </BrowserRouter>
+        <main>
+            <BrowserRouter>
+                <NavMenu />
+                <header className={styles.header}>
+                    <div className={styles.header__text}>
+                        A casa do código e da massa
+                    </div>
+                </header>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/menu' element={<Cardapio />} />
+                </Routes>
+            </BrowserRouter>
+        </main>
     );
 }
