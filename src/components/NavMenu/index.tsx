037376042a4a146b079c02e332cfeb3c65575368
Samilly <samillynunes19@gmail.com>
2025-04-11 10:45:22 -0300

    feat: links inside NavMenu

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -2,11 +2,33 @@
 import { ReactComponent as Logo } from 'assets/logo.svg';
 
 import styles from './NavMenu.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function NavMenu(){
+    const routes = [{
+      label: 'Início',
+      to: '/',
+    },{
+      label: 'Cardápio',
+      to: '/menu',
+    },{
+      label: 'Sobre',
+      to: '/about',
+    }
+  ];
+
     return (
         <nav className={styles.menu}>
         <Logo />
+        <ul className={styles.menuList} >
+          {routes.map((route, index) => (
+            <li key={index} className={styles.menuLink}>
+              <Link to={route.to} >
+                {route.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     );
 }
