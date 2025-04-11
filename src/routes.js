618f79cf96311e5c23c21a9602a2273a1d583fdc
Samilly <samillynunes19@gmail.com>
2025-04-11 11:00:32 -0300

    refactor: using nested routes

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,21 @@
+import Banner from 'components/Banner';
 import NavMenu from 'components/NavMenu';
 import Cardapio from 'pages/Cardapio';
 import Home from 'pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import styles from './Routes.module.scss';
 
 export default function AppRouter() {
     return (
         <main>
             <BrowserRouter>
                 <NavMenu />
-                <header className={styles.header}>
-                    <div className={styles.header__text}>
-                        A casa do código e da massa
-                    </div>
-                </header>
+
                 <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/menu' element={<Cardapio />} />
+                    <Route path='/' element={<Banner />}>
+                        <Route index element={<Home/>} />
+                        <Route path='menu' element={<Cardapio />} />
+
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </main>
